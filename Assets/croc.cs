7c4a813c23b3459f2e79c1c29b5d4ca4ece73552
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2025-10-15 08:36:09 +0200

    2025-10-14

--- Assets/croc.cs
+++ Assets/croc.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class croc : MonoBehaviour
 {
 
+
+    [SerializeField]
+    float speed = 3.5f;
+
+    [SerializeField]
+    float jumpforce = 8.5f;
+
     [SerializeField]
     LayerMask groundlayer;
+
+    [SerializeField]
+    GameObject groundCheck;
+
+    
+    void Start()
+    {
+       
+    }
+
     void FixedUpdate()
     {
 
-        bool isgrounded = Physics2D.OverlapCircle(chec.transform.position, .08f, groundlayer);
+        bool isgrounded = Physics2D.OverlapCircle(groundCheck.transform.position, .18f, groundlayer);
 
 
         if (Input.GetAxisRaw("Jump") > 0 && isgrounded == true)
@@ -18,10 +36,15 @@ public class croc : MonoBehaviour
             rb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
         }
 
-        // Update is called once per frame
-        void Update()
-        {
 
-        }
+    }
+      void Update()
+
+
+    {
+
+        Vector2 movement = Vector2.left * speed * Time.deltaTime;
+        transform.Translate(movement);
+
     }
 }
